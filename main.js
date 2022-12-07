35596eed17606049e95cfd911fc734f9a4feee89
ilyass atlassi <atlassiilyass@gmail.com>
2022-12-07 22:48:37 +0100

    Add the constructor

--- main.js
+++ main.js
@@ -1,39 +1,12 @@
 let booksList = [];
 
-const form = document.querySelector('#form');
+// const form = document.querySelector('#form');
 const bookContainer = document.querySelector('.box');
 
-const getBookData = () => {
-  let books;
-  if (localStorage.getItem('Data') != null) {
-    books = JSON.parse(localStorage.getItem('Data'));
-  } else {
-    books = booksList;
+class Books {
+    constructor(title, author = null, id) {
+      this.id = id;
+      this.title = title;
+      this.author = author;
+    }
   }
-  return books;
-};
-
-const setBook = (book) => {
-  const books = getBookData();
-  books.push(book);
-  booksList = books;
-  localStorage.setItem('Data', JSON.stringify(books));
-};
-
-const removeBook = (element) => {
-  const books = getBookData();
-  const position = Array.prototype.indexOf.call(
-    bookContainer.childNodes,
-    element.parentElement,
-  ) - 1;
-
-  if (element.classList.contains('button')) {
-    books.forEach((book, index) => {
-      if (position === index) {
-        books.splice(index, 1);
-      }
-      booksList = books;
-      localStorage.setItem('Data', JSON.stringify(books));
-    });
-  }
-};
